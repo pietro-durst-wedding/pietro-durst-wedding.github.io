a7ba78d0d0988425d32e8a38445370d3ebe00614
robertDurst <me@robdurst.com>
2022-05-23 02:00:36 -0400

    add wedding countdown at the bottom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './App.css';
 import BasicTabs from './components/Tabs'
 import BasicRouter from './components/Router'
+import Countdown from 'react-countdown';
 
 function App() {
   const [tabValue, setTabValue] = React.useState('home');
@@ -9,6 +10,10 @@ function App() {
   const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
     setTabValue(newValue);
   };
+
+  const renderer = ({ days, hours, minutes }) => (
+    days + " days and " + hours + " hours until the wedding!"
+  );
 
   return (
     <div className="App">
@@ -28,7 +33,14 @@ function App() {
         {/* </div> */}
         {/* <div className='right-middle'></div> */}
       </div>
-      <div className='bottom'></div>
+      <div className='bottom'>
+      <Countdown
+        date={new Date('April 29, 2023 12:00:00')}
+        intervalDelay={0}
+        precision={0}
+        renderer={props => <div>{renderer(props)}</div>}
+      />
+      </div>
     </div>
   );
 }
